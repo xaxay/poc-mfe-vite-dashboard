fce4d493e79c1fa9956fb28bae256a9b589e2462
mpopel <mikhail.popel@gmail.com>
2024-07-23 21:05:03 +0300

    initial commit

---
+++ src/shims/shim-api-nav.d.ts
@@ -0,0 +1,9 @@
+// /src/shims/shim-api-nav.d.ts
+
+declare module "@browser-module/api/nav" {
+  import { RouteLocationRaw, RouteLocationNormalizedLoaded } from 'vue-router';
+
+  export function navigateTo(to: RouteLocationRaw): void;
+  export function getCurrentRoute() : RouteLocationNormalizedLoaded;
+  export function join(...paths: string[]): string;
+}
